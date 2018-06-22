1a6f29d0e96629a8a477ac51fa7352a8d2fe0d51
owen <panrh@huihangtech.com>
2018-06-22 17:04:22 +0800

    前后台基本结构

--- src/component/nav.vue
+++ src/component/nav.vue
@@ -1,9 +1,32 @@
 <template>
-    <header>
-        <!-- router-link 定义点击后导航到哪个路径下 -->
-        <router-link to="/home">Home</router-link>
-        <router-link to="/hello">hello</router-link>
-    </header>
+
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <!-- Brand -->
+        <a class="navbar-brand" href="#">Navbar</a>
+
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <router-link to="/home" class="nav-link">Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/hello" class="nav-link">hello</router-link>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 <script>
 
