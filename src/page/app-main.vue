6c3232b1793f21eda8c69d567d3b65d14370c3ae
owen <1@2.3>
2018-07-23 09:33:51 +0800

    global turn

--- src/page/app-main.vue
+++ src/page/app-main.vue
@@ -3,25 +3,20 @@
         <PNav ref="navbar"></PNav>
         <!-- 对应的组件内容渲染到router-view中 -->
         <router-view @onViewIn="viewIned" @onShadowBgShow="shadowBgShow"> </router-view>
-        <div v-if="showShadowBg" class="modal-backdrop fade show" @click="showShadowBg(false)"></div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            text: "app",
-            showShadowBg:false
+         
         };
     },
     methods: {
         viewIned() {
             this.$refs.navbar.collapse();
         },
-        shadowBgShow(isOk){
-            console.log(isOk)
-            this.showShadowBg=isOk;
-        }
+        
     },
     mounted() {}
 };
--- src/page/main/cardedit.vue
+++ src/page/main/cardedit.vue
@@ -3,39 +3,56 @@
 
         <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
-                <strong class="d-inline-block mb-2 text-success">管理</strong>
+                <strong class="d-inline-block mb-2 text-success w-100">
+                    管理
+                    <button class="btn btn-sm btn-light float-right">
+                        <router-link to="/cards">
+                            <svg height="16" class="octicon octicon-mirror" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M15.5 4.7L8.5 0l-7 4.7c-.3.19-.5.45-.5.8V16l7.5-4 7.5 4V5.5c0-.34-.2-.61-.5-.8zm-.5 9.8l-6-3.25V10H8v1.25L2 14.5v-9l6-4V6h1V1.5l6 4v9zM6 7h5V5l3 3-3 3V9H6v2L3 8l3-3v2z"></path>
+                            </svg>
+                        </router-link>
+                    </button>
+                    <button class="btn btn-sm btn-light float-right mr-1">
+                        <router-link to="/cardtrash">
+                            <svg height="16" class="octicon octicon-trashcan" viewBox="0 0 12 16" version="1.1" width="16" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M11 2H9c0-.55-.45-1-1-1H5c-.55 0-1 .45-1 1H2c-.55 0-1 .45-1 1v1c0 .55.45 1 1 1v9c0 .55.45 1 1 1h7c.55 0 1-.45 1-1V5c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H3V5h1v8h1V5h1v8h1V5h1v8h1V5h1v9zm1-10H2V3h9v1z"></path>
+                            </svg>
+                        </router-link>
+                    </button>
+                </strong>
                 <h3 class="mb-0">
-                    <a class="text-dark">清单</a>
+                    <a class="text-dark">清单</a>&nbsp;
+                    <button class="btn btn-sm btn-outline-success float-right" @click="showModal(null)">新增</button>
                 </h3>
                 <hr>
                 <table class="table">
                     <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">名称</th>
-                            <th scope="col">日期</th>
-                            <th scope="col">操作</th>
-                        </tr>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">名称</th>
+                        <th scope="col">日期</th>
+                        <th scope="col">操作</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="(it,idx) in videos" :key="it.id">
-                            <th scope="row">{{idx+1}}</th>
-                            <td>{{it.title}}</td>
-                            <td>{{parseWeek(it.week)}}</td>
-                            <td>
-                                <button class="btn btn-outline-info" @click="showModal(idx)">修改</button>
-                            </td>
-                        </tr>
+                    <tr v-for="(it,idx) in videos" :key="it.id">
+                        <th scope="row">{{idx+1}}</th>
+                        <td>{{it.title}}</td>
+                        <td>{{parseWeek(it.week)}}</td>
+                        <td>
+                            <button class="btn btn-outline-info" @click="showModal(idx)">修改</button>
+                        </td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
-            <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164ac640af6%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164ac640af6%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.203125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 200px; height: 250px;">
         </div>
-        <div class="modal fade" :class="isShow?'show':''" id="theModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+
+        <div class="modal fade" id="theModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                        <h5 class="modal-title">{{isAdd?"新增":"修改"}}</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -55,43 +72,45 @@
 </template>
 <script>
     import config from "../../asserts/util/config";
-export default {
-    data() {
-        return {
-            videos: [],
-            isShow: false
-        };
-    },
-    computed: {},
-    methods: {
-        parseWeek(idx) {
-            const label = [, "一", "二", "三", "四", "五", "六", "日"];
-            return "周" + label[idx];
+    export default {
+        data() {
+            return {
+                videos: [],
+                isAdd: true
+            };
         },
-        getList() {
-            this.$axios({
-                method: "post",
-                url: config.baseApiUrl + "?m=VideoController!getVideosByStatus",
-                data: "status=0"
-            }).then(it => {
-                this.videos = it.data.obj;
-            });
+        computed: {},
+        methods: {
+            parseWeek(idx) {
+                const label = [, "一", "二", "三", "四", "五", "六", "日"];
+                return "周" + label[idx];
+            },
+            getList() {
+                this.$axios({
+                    method: "post",
+                    url: config.baseApiUrl + "?m=VideoController!getVideosByStatus",
+                    data: "status=0"
+                }).then(it => {
+                    this.videos = it.data.obj;
+                });
+            },
+            showModal(idx) {
+                $("#theModal").modal("show");
+                if (idx !== null) {
+                    this.isAdd = false;
+                } else {
+                    this.isAdd = true;
+                }
+            }
         },
-        showModal(idx) {
-            // this.isShow = true;
-            $("#theModal").modal("show");
-            setTimeout(()=>{this.isShow=true},1000);
-            this.$emit("onShadowBgShow", true);
+        mounted() {
+            this.$emit("onViewIn", this.items);
+            this.getList();
         }
-    },
-    mounted() {
-        this.$emit("onViewIn", this.items);
-        this.getList();
-    }
-};
+    };
 </script>
 <style>
-.modal.fade.show {
-    display: block;
-}
+    .modal.fade.show {
+        display: block;
+    }
 </style>
